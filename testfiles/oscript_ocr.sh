dd66b366bf019d168a6d743e75a825f09d187f3a
Duc Pham Le <ducphamle212@gmail.com>
2020-11-14 21:02:57 +0700

    fix: fix output of ocr oracle script

--- testfiles/oscript_ocr.sh
+++ testfiles/oscript_ocr.sh
@@ -10,7 +10,18 @@ route() {
     echo "testcase_ocr"
   elif [[ $1 = "aggregation" ]] # $2 is true output, $3 is expected output
   then
-    echo $2
+    # collect input string with a delimiter of each data source value separated with a '-' delimiter
+    IFS='-' read -ra array <<< "$2"
+    echo $IFS
+    aggregation_result=0
+    size=0
+    result=""
+    # here is the algorithm for each oracle script. This should be different based on the oscript
+    for i in "${array[@]}"; do
+      let "size+=1"
+    done
+    echo "collected the following result from" $size "data sources that passed the test case": ${array[0]}
+    #echo $2
   else
     echo 0
   fi
