e5d7886eaa202f531e7e0b102456acd482153741
Le Duc Pham <ducphamle212@gmail.com>
2020-12-24 22:52:09 +0700

    update: finish create validator flow

--- fn.sh
+++ fn.sh
@@ -226,7 +226,7 @@ initFn(){
     # [ ! -z $persistentPeers ] && sed -i 's/persistent_peers *= *".*"/persistent_peers = "$PERSISTENT_PEERS"/g' .oraid/config/config.toml 
 
     # add persistent peers to listen to blocks
-    sed -i 's/persistent_peers *= *".*"/persistent_peers = "dbb2fee9a55cbbe364afcf377cb2f82882fa4ad6@164.90.180.95:26656,a9f2161cb9d723c26e8f268a660b5cc1261e264a@157.230.22.169:26656,950e189e74134d9341d859c6132f5d4da7c19b58@165.232.118.44:26656,c5bcf6dd8efa1a2ee0efdec4a18da8d3a2654302@178.128.61.252:26656,4969be3fdf0e20c77ee252fb156d8bf2a9496317@178.128.57.195:26656,5efef6b1af1429a8c4ae30ed568d7fe97cf2f0c0@159.89.206.139:26656,3076fd764d2eda73bc3637d84549dff587897e51@178.128.220.155:26656"/g' .oraid/config/config.toml
+    sed -i 's/persistent_peers *= *".*"/persistent_peers = "8bd5a1689706f72a595ad2cb3ea09ad7f9a47eb9@209.97.154.247:26656"/g' .oraid/config/config.toml
 
     oraid validate-genesis
     # done init
@@ -303,16 +303,51 @@ createValidatorFn() {
   local amount=$(getArgument "amount" $AMOUNT)
   local pubkey=$(oraid tendermint show-validator)
   local moniker=$(getArgument "moniker" $MONIKER)
+  if [[ $moniker == "" ]]; then
+    moniker="$USER"_"Oraichain"_$(($RANDOM%100000000000))
+  fi
+  local floatRe='^[+-]?[0-9]+\.?[0-9]*$'
   local commissionRate=$(getArgument "commission_rate" $COMMISSION_RATE)
+  if [[ $commissionRate > 1 || !$commissionRate =~ $floatRe || $commissionRate == "" ]]; then
+    commissionRate=0.10
+  fi
   local commissionMaxRate=$(getArgument "commission_max_rate" $COMMISSION_MAX_RATE)
+  if [[ $commissionMaxRate > 1 || !$commissionMaxRate =~ $floatRe || commissionMaxRate == "" ]]; then
+    commissionMaxRate=0.20
+  fi
   local commissionMaxChangeRate=$(getArgument "commission_max_change_rate" $COMMISSION_MAX_CHANGE_RATE)
+  if [[ $commissionMaxChangeRate > 1 || !$commissionMaxChangeRate =~ $floatRe || $commissionMaxChangeRate == "" ]]; then
+    commissionMaxChangeRate=0.01
+  fi
   local minDelegation=$(getArgument "min_self_delegation" $MIN_SELF_DELEGATION)
-  local gas=$(getArgument "gas" "auto")
+
+  # verify env from user, regex for checking number
+  local re='^[0-9]+$'
+  if [[ $minDelegation < 1 || !$minDelegation =~ $re || $minDelegation == "" ]]; then
+    minDelegation=1
+  fi
+
+  local gas=$(getArgument "gas" $GAS)
+  if [[ $gas != "auto" && !$gas =~ $re ]]; then
+    gas=200000
+  fi
+
+  # workaround, since auto gas in this case is not good, sometimes get out of gas
+  if [[ $gas == "auto" || $gas < 200000 ]]; then
+    gas=200000
+  fi
+
   local gasPrices=$(getArgument "gas_prices" $GAS_PRICES)
+  if [[ $gasPrices == "" ]]; then
+    gasPrices="0.025orai"
+  fi
   local securityContract=$(getArgument "security_contract" $SECURITY_CONTRACT)
   local identity=$(getArgument "identity" $IDENTITY)
   local website=$(getArgument "website" $WEBSITE)
   local details=$(getArgument "details" $DETAILS)
+
+  sleep 10
+
   echo "y" | oraicli tx staking create-validator --amount $amount --pubkey $pubkey --moniker $moniker --chain-id Oraichain --commission-rate $commissionRate --commission-max-rate $commissionMaxRate --commission-max-change-rate $commissionMaxChangeRate --min-self-delegation $minDelegation --gas $gas --gas-prices $gasPrices --from $user
 
   local reporter="${user}_reporter"
@@ -346,12 +381,12 @@ createValidatorFn() {
   # config log type
   $WEBSOCKET config log-level debug
 
-  sleep 2
+  sleep 10
 
   # send orai tokens to reporters
   echo "y" | oraicli tx send $(oraicli keys show $user -a) $($WEBSOCKET keys show $reporter) 10000000orai --from $(oraicli keys show $user -a) --fees 5000orai
 
-  sleep 6
+  sleep 10
 
   #wait for sending orai tokens transaction success
 
@@ -362,7 +397,7 @@ createValidatorFn() {
   pkill oraid
 }
 
-USER=$(getArgument "user" duc)
+USER=$(getArgument "user" $USER)
 
 # processing
 case "${METHOD}" in     
