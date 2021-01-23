0b79ea330af7e12989dbfb85214ebd985a0e502e
Le Duc Pham <ducphamle212@gmail.com>
2021-01-23 11:01:42 +0000

    update: change keeper params to pointer

--- x/airequest/keeper/params.go
+++ x/airequest/keeper/params.go
@@ -28,18 +28,18 @@ func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 */
 
 // GetParam returns the parameter as specified by key as an uint64.
-func (k Keeper) GetParam(ctx sdk.Context, key []byte) (res uint64) {
+func (k *Keeper) GetParam(ctx sdk.Context, key []byte) (res uint64) {
 	k.paramSpace.Get(ctx, key, &res)
 	return res
 }
 
 // SetParam saves the given key-value parameter to the store.
-func (k Keeper) SetParam(ctx sdk.Context, key []byte, value uint64) {
+func (k *Keeper) SetParam(ctx sdk.Context, key []byte, value uint64) {
 	k.paramSpace.Set(ctx, key, value)
 }
 
 // GetParams returns all current parameters as a types.Params instance.
-func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+func (k *Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 	k.paramSpace.GetParamSet(ctx, &params)
 	return params
 }
