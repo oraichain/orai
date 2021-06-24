62b16f8dd13725b4ff1ffa99f6cb93c739839665
tuorai <tubackkhoa@gmail.com>
2021-06-24 11:09:07 +0700

    fix protocgen

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+# set -eo pipefail
 
 BASEDIR=$(dirname $0)
 PROJECTDIR=$BASEDIR/..
@@ -9,15 +9,15 @@ COSMOS_SDK_DIR=${COSMOS_SDK_DIR:-$(go list -f "{{ .Dir }}" -m github.com/cosmos/
 
 # Generate Go types from protobuf
 for PROTO_DIR in $(ls $MODULE_SDK_DIR)
-do   
+do
   echo "processing: $MODULE_SDK_DIR/$PROTO_DIR"
-  proto_files=$(find "x/$PROTO_DIR/types/" -maxdepth 4 -name '*.proto')  
+  proto_files=$(find "x/$PROTO_DIR/types/" -maxdepth 4 -name '*.proto')
   buf protoc \
     -I=. \
     -I=$COSMOS_SDK_DIR/third_party/proto \
     -I=$COSMOS_SDK_DIR/proto \
     --gocosmos_out=Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,plugins=interfacetype+grpc,paths=source_relative:. \
+    --grpc-gateway_out=Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,paths=source_relative:. \
     $proto_files
 done
-
 
