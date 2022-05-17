9b5d7d67bd58eab4564c62e9cadaf585a73755cb
Pham Tu <tubackkhoa@gmail.com>
2022-05-17 10:17:04 +0700

    update genproto

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -5,7 +5,7 @@
 BASEDIR=$(dirname $0)
 PROJECTDIR=$BASEDIR/..
 MODULE_SDK_DIR=$(realpath $PROJECTDIR/x)
-COSMOS_SDK_DIR=${COSMOS_SDK_DIR:-$(go list -f "{{ .Dir }}" -m github.com/cosmos/cosmos-sdk)}
+COSMOS_SDK_DIR="cosmos-sdk" # ${COSMOS_SDK_DIR:-$(go list -f "{{ .Dir }}" -m github.com/cosmos/cosmos-sdk)}
 
 # Generate Go types from protobuf
 for PROTO_DIR in $(ls $MODULE_SDK_DIR)
@@ -13,11 +13,15 @@ do
   echo "processing: $MODULE_SDK_DIR/$PROTO_DIR"
   proto_files=$(find "x/$PROTO_DIR/types/" -maxdepth 4 -name '*.proto')
   buf protoc \
-    -I=. \
+    -I=$MODULE_SDK_DIR \
     -I=$COSMOS_SDK_DIR/third_party/proto \
     -I=$COSMOS_SDK_DIR/proto \
     --gocosmos_out=Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,plugins=interfacetype+grpc,paths=source_relative:. \
     --grpc-gateway_out=Mgoogle/protobuf/any.proto=github.com/cosmos/cosmos-sdk/codec/types,Mgoogle/protobuf/empty.proto=github.com/gogo/protobuf/types,paths=source_relative:. \
     $proto_files
+
+  # move proto files to the right places
+  cp -r $PROTO_DIR/* $MODULE_SDK_DIR/$PROTO_DIR
+  rm -rf $PROTO_DIR
 done
 
