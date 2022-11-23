805ea15fd50c0415f3d07270973934ac32fb27c0
Pham Thanh Tu <tubackkhoa@gmail.com>
2022-11-23 17:26:43 +0700

    Delete cosmos-sdk directory

--- cosmos-sdk/x/slashing/init_test.go
+++
@@ -1,9 +0,0 @@
-package slashing_test
-
-import (
-	sdk "github.com/cosmos/cosmos-sdk/types"
-)
-
-var (
-	InitTokens = sdk.TokensFromConsensusPower(200)
-)
--- cosmos-sdk/x/slashing/keeper/common_test.go
+++
@@ -1,7 +0,0 @@
-package keeper_test
-
-import sdk "github.com/cosmos/cosmos-sdk/types"
-
-var (
-	InitTokens = sdk.TokensFromConsensusPower(200)
-)
