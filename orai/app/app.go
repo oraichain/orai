903e43fe56410824c21a77e8c3cec5213bbf6548
ducphamle2 <ducphamle212@gmail.com>
2024-02-28 21:58:24 -0800

    fix: re-add added empty store upgrade

--- orai/app/app.go
+++ orai/app/app.go
@@ -1045,7 +1045,9 @@ func (app *OraichainApp) upgradeHandler() {
 
 	if upgradeInfo.Name == BinaryVersion && !app.upgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
-		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storetypes.StoreUpgrades{}))
+		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storetypes.StoreUpgrades{
+			Added: []string{},
+		}))
 	}
 }
 
