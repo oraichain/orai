8ceabcd46671783262b695fd2d3c8e12a7efaa8b
ducphamle2 <ducphamle212@gmail.com>
2024-02-28 21:38:58 -0800

    pump docker image & binary version

--- orai/app/app.go
+++ orai/app/app.go
@@ -145,7 +145,7 @@ const appName = "Oraichain"
 var (
 	NodeDir = ".oraid"
 
-	BinaryVersion = "v0.41.5"
+	BinaryVersion = "v0.41.7"
 
 	// If EnabledSpecificProposals is "", and this is "true", then enable all x/wasm proposals.
 	// If EnabledSpecificProposals is "", and this is not "true", then disable all x/wasm proposals.
