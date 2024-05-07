ae8ebf87cf0d8d5a6906268d8867535ce7cdbaf9
ducphamle2 <ducphamle212@gmail.com>
2024-05-07 14:07:39 -0700

    use ethermint enccodec

--- orai/app/encoding.go
+++ orai/app/encoding.go
@@ -1,7 +1,6 @@
 package app
 
 import (
-	"github.com/cosmos/cosmos-sdk/std"
 	appparams "github.com/oraichain/orai/app/params"
 	enccodec "github.com/tharsis/ethermint/encoding/codec"
 )
@@ -9,8 +8,6 @@ import (
 // MakeEncodingConfig creates an EncodingConfig for gravity.
 func MakeEncodingConfig() appparams.EncodingConfig {
 	encodingConfig := appparams.MakeEncodingConfig()
-	std.RegisterLegacyAminoCodec(encodingConfig.Amino)
-	std.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	enccodec.RegisterLegacyAminoCodec(encodingConfig.Amino)
 	enccodec.RegisterInterfaces(encodingConfig.InterfaceRegistry)
 	ModuleBasics.RegisterLegacyAminoCodec(encodingConfig.Amino)
